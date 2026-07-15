63f5d7337dcab54dce966e94bacfa8d941b83547
Vishaal <vishaal2007.m@gmail.com>
2026-07-15 16:09:06 +0530

    Main - Rename Flutter Demo - Fashion App

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Fashion App',
       theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
       home: NavigationScreen(),
     );
